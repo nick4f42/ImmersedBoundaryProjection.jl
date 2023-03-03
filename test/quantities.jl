0f5a3e98d74665943d1e7c8a6247acff80c3653b
Nick OBrien <nick4f42@proton.me>
2023-03-03 15:07:28 -0600

    Improve quantity plotting tests
    Since the tested quantities are array types, they might have default
    behavior in `Plots`. The tests now properly check that the plotting
    recipe was applied.

--- test/quantities.jl
+++ test/quantities.jl
@@ -9,13 +9,18 @@ using Plots
         let xs = LinRange(0, 1, 3), ys = LinRange(0, 2, 4)
             z = xs .+ ys'
             val = GridValue(z, (xs, ys))
-            @test plot(val) isa Any
+
+            p, = plot(val)
+            @test p[1][:seriestype] == :heatmap # Make sure the recipe was applied
         end
 
         let coords = [(LinRange(-t, t, 6), LinRange(-0.5t, 0.5t, 3)) for t in 2.0 .^ (0:2)]
             z = cat((xs .+ ys' for (xs, ys) in coords)...; dims=3)
             val = MultiLevelGridValue(z, coords)
-            @test plot(val) isa Any
+
+            p, = plot(val)
+            @test p[1][:seriestype] == :heatmap # Make sure the recipe was applied
+            @test length(p.series_list) == size(z, 3) # One heatmap per sublevel
         end
     end
 end
